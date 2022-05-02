8c645be989532cd0a12e056a90b1acf1933aa935
Tony <mr.landero@gmail.com>
2022-05-02 14:09:33 -0500

    Add logic to option b: sell your car

--- Dealership.java
+++ Dealership.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Dealership {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to the Java Dealership!");
+        System.out.println("\nWelcome to the Java Dealership!");
         System.out.println(" - Select option 'a' to buy a car.");
         System.out.println(" - Select option 'b' to sell a car.");
         String option = scan.nextLine();
         switch (option) {
             case "a": 
-                System.out.println("What is your budget?"); 
+                System.out.println("\nWhat is your budget?"); 
                 int budget = scan.nextInt();
                 if (budget >= 10000) {
-                    System.out.println("Great! A Nissan Altima is available.");
+                    System.out.println("\nGreat! A Nissan Altima is available.");
                     System.out.println("\nDo you have insurance? Type 'yes' or 'no'");
                     scan.nextLine();
                     String insurance = scan.nextLine();
@@ -21,16 +21,26 @@ public class Dealership {
                     System.out.println("\nWhat is your credit score?");
                     int creditScore = scan.nextInt();
                     if (insurance.equals("yes") && license.equals("yes") && creditScore >= 660) {
-                        System.out.println("Sold! Pleasure doing business with you.");
+                        System.out.println("\nSold! Pleasure doing business with you.");
                     } else {
-                        System.out.println("We're sorry! You don't qualify to buy this car.");
+                        System.out.println("\nWe're sorry! You don't qualify to buy this car.");
                     }
                 } else {
-                    System.out.println("We don't sell cars under $10,000. Sorry!");
+                    System.out.println("\nWe don't sell cars under $10,000. Sorry!");
                 }
                 break;
-            case "b": System.out.println("You chose option " + option); break;
-            default: System.out.println("That's an invalid option.");
+            case "b": 
+                System.out.println("\nWhat is the value of your car?");
+                int value = scan.nextInt();
+                System.out.println("\nWhat is your selling price?");
+                int sellingPrice = scan.nextInt();
+                if (value > sellingPrice && sellingPrice < 30000) {
+                    System.out.println("\nWe will buy your car. Pleasure doing business with you.");
+                } else {
+                    System.out.println("\nSorry! We're not interested in buying your car.");
+                }
+                break;
+            default: System.out.println("\nThat's an invalid option.");
         }
         scan.close();
     }
